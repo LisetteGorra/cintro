11814f0a1d0391cbb35a761abb5cb78b87883091
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-04-24 00:07:31 +0000

    used threeSquared, squared num, and multiplied num

--- hello.c
+++ hello.c
@@ -2,22 +2,30 @@
 #include <cs50.h>
 #include <stdio.h>
 
-
 void printSum(void);
-void printSums(int , int);
+void printSums(int, int);
+int squared(int);
 
 int main(void)
 {
     //print format with new line symbol
     printf("hello\n");
     printSums(4, 8);
+    int threeSquared = squared(3);
+    printf("3 squared is: %i\n", threeSquared);
 }
+
+int squared(num)
+{
+    return num * num;
+}
+
 void printSum(void)
 
 {
     printf("1 + 2 = %i\n", 1 + 2);
 }
-void printSums(int x, int y)
+void printSums(x, y)
 {
     printf("%i + %i = %i\n", x, y, x + y);
 }
