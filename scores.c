f1531b655a2676926b72d1d66168a658a21ffca7
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-05-13 17:07:13 +0000

    Created function so that copy and past of for loop isnt necessary

--- scores.c
+++ scores.c
@@ -1,6 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void chart(int score)
+{
+    for (int i = 0; i < score; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+
+}
+
 int main(void)
 {
     int Score1 = get_int("Score 1: ");
@@ -8,23 +18,58 @@ int main(void)
     int Score3 = get_int("Score 3: ");
 
     printf("Score1");
+    chart(Score1);
+
+    printf("Score2");
+    chart(Score2);
+
+    printf("Score3");
+    chart(Score3);
+
+
     for (int i = 0; i < Score1; i++)
     {
         printf("#");
     }
     printf("\n");
 
-    printf("Score2");
-    for (int i = 0; i < Score2; i++)
-    {
-        printf("#");
-    }
-    printf("\n");
 
-    printf("Score3");
-    for (int i = 0; i < Score3; i++)
-    {
-        printf("#");
-    }
-    printf("\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // printf("Score2");
+    // for (int i = 0; i < Score2; i++)
+    // {
+    //     printf("#");
+    // }
+    // printf("\n");
+
+    // printf("Score3");
+    // for (int i = 0; i < Score3; i++)
+    // {
+    //     printf("#");
+    // }
+    // printf("\n");
 }
